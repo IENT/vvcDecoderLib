6ed46b2568c48e3b292e21e63e53418fe239232a
Karsten Suehring <karsten.suehring@hhi.fraunhofer.de>
2018-05-08 14:03:48 +0000

    compile fixes for gcc

--- source/Lib/DecoderLib/CABACReader.cpp
+++ source/Lib/DecoderLib/CABACReader.cpp
@@ -2679,7 +2679,9 @@ void CABACReader::cu_chroma_qp_offset( CodingUnit& cu )
 
 void CABACReader::residual_coding( TransformUnit& tu, ComponentID compID )
 {
+#if ENABLE_TRACING || HEVC_USE_SIGN_HIDING || JEM_TOOLS
   const CodingUnit& cu = *tu.cu;
+#endif
   DTRACE( g_trace_ctx, D_SYNTAX, "residual_coding() etype=%d pos=(%d,%d) size=%dx%d predMode=%d\n", tu.blocks[compID].compID, tu.blocks[compID].x, tu.blocks[compID].y, tu.blocks[compID].width, tu.blocks[compID].height, cu.predMode );
 
   // parse transform skip and explicit rdpcm mode
--- source/Lib/EncoderLib/CABACWriter.cpp
+++ source/Lib/EncoderLib/CABACWriter.cpp
@@ -2840,7 +2840,9 @@ void CABACWriter::cu_chroma_qp_offset( const CodingUnit& cu )
 
 void CABACWriter::residual_coding( const TransformUnit& tu, ComponentID compID )
 {
+#if ENABLE_TRACING || HEVC_USE_SIGN_HIDING || JEM_TOOLS
   const CodingUnit& cu = *tu.cu;
+#endif
   DTRACE( g_trace_ctx, D_SYNTAX, "residual_coding() etype=%d pos=(%d,%d) size=%dx%d predMode=%d\n", tu.blocks[compID].compID, tu.blocks[compID].x, tu.blocks[compID].y, tu.blocks[compID].width, tu.blocks[compID].height, cu.predMode );
 
   // code transform skip and explicit rdpcm mode
