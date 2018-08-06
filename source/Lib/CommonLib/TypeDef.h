b6381e2f5cf38187caee34cd85ed193616ab26de
YanZhang <yzh@qti.qualcomm.com>
2018-08-06 20:04:29 +0800

    Integration of JVET-K0351

--- source/Lib/CommonLib/TypeDef.h
+++ source/Lib/CommonLib/TypeDef.h
@@ -72,7 +72,7 @@
 
 #define JVET_K0346                                        1 // simplifications on ATMVP
 
-#define JVET_K0351_RELEASE_TT_CENTER_RESTRICTION          0 // allowing binary partition for the center block of a tenary partitioned CU
+#define JVET_K0351_RELEASE_TT_CENTER_RESTRICTION          1 // allowing binary partition for the center block of a tenary partitioned CU
 
 #ifndef JEM_TOOLS
 #define JEM_TOOLS                                         1 // Defines the inclusion of JEM tools into compiled executable
