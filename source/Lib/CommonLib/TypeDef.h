3a7179b1108a91116224d75856d85f8cb4614c92
Johannes Sauer <sauer@ient.rwth-aachen.de>
2018-09-01 03:16:53 +0200

    K0149 blockstatistics

--- source/Lib/CommonLib/TypeDef.h
+++ source/Lib/CommonLib/TypeDef.h
@@ -166,7 +166,12 @@
 
 #ifndef ENABLE_TRACING
 #define ENABLE_TRACING                                    0 // DISABLE by default (enable only when debugging, requires 15% run-time in decoding) -- see documentation in 'doc/DTrace for NextSoftware.pdf'
-
+#if ENABLE_TRACING
+#define K0149_BLOCK_STATISTICS                            1 // enables block statistics, which can be analysed with YUView (https://github.com/IENT/YUView)
+#if K0149_BLOCK_STATISTICS
+#define BLOCK_STATS_AS_CSV                                0 // statistics will be written in a comma separated value format. this is not supported by YUView
+#endif
+#endif
 #endif // ! ENABLE_TRACING
 
 #define WCG_EXT                                           0 // part of JEM sharp Luma qp
